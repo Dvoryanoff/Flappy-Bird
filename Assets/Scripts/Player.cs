5787fe4b68c3d29a5637f5ba5247fe0739ac16a2
Dvoryanoff <dvoryanoff@mail.ru>
2022-06-17 10:40:34 +0300

    Score + hit the obstacles triggers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -46,4 +46,13 @@ public class Player : MonoBehaviour
 
         spriteRenderer.sprite = sprites[spriteIndex];
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle")) {
+            FindObjectOfType<GameManager>().Gameover();
+        } else if (other.gameObject.CompareTag("Scoring")) {
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
+    }
 }
