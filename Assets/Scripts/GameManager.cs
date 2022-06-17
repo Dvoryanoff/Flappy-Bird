e35447fdb6604f73a95e4879c7ddad145824fbc8
Dvoryanoff <dvoryanoff@mail.ru>
2022-06-17 11:46:33 +0300

    Game Done!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,51 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Player player;
     private int score;
+    public TMP_Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+        Time.timeScale = 1;
+        player.enabled = true;
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        for (int i = 0; i < pipes.Length; i++) {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        player.enabled = false;
+    }
 
     public void Gameover()
     {
-        Debug.Log("Game Over!");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 }
